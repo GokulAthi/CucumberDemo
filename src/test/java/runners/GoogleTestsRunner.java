6defdad8d071ed1412dc37d5a8afc3bd27217475
GokulAthi <goku2812@gmail.com>
2020-08-30 16:30:10 +0530

    Reports

--- src/test/java/runners/GoogleTestsRunner.java
+++ src/test/java/runners/GoogleTestsRunner.java
@@ -7,7 +7,7 @@ import org.junit.runner.RunWith;
 @RunWith(Cucumber.class)
 @CucumberOptions(features="src/test/resources/features",
         glue="stepdefinitions",
-        plugin={"html:target/cucumber-html-report", "json:target/cucumber.json",
+        plugin={"html:target/cucumber-html-report.html", "json:target/cucumber.json",
                 "pretty:target/cucumber-pretty.txt","usage:target/cucumber-usage.json",
                 "junit:target/cucumber-results.xml"},
         monochrome = true
